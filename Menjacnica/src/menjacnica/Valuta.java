0e22cd310a79f73b3849ec5912d725c73a6eef33
ljuba95 <ljuba95@hotmail.com>
2016-03-17 22:14:33 +0100

    Kreirana klasa Valuta, get i set metode bez logicke provere

--- Menjacnica/src/menjacnica/Valuta.java
+++ Menjacnica/src/menjacnica/Valuta.java
@@ -1,5 +1,51 @@
 package menjacnica;
 
-public class Valuta {
+import java.util.GregorianCalendar;
 
+public class Valuta {
+	
+	private String naziv, kraciNaziv;
+	private double prodajni, kupovni, srednji;
+	private GregorianCalendar datum;
+	
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public String getKraciNaziv() {
+		return kraciNaziv;
+	}
+	public void setKraciNaziv(String kraciNaziv) {
+		this.kraciNaziv = kraciNaziv;
+	}
+	public double getProdajni() {
+		return prodajni;
+	}
+	public void setProdajni(double prodajni) {
+		this.prodajni = prodajni;
+	}
+	public double getKupovni() {
+		return kupovni;
+	}
+	public void setKupovni(double kupovni) {
+		this.kupovni = kupovni;
+	}
+	public double getSrednji() {
+		return srednji;
+	}
+	public void setSrednji(double srednji) {
+		this.srednji = srednji;
+	}
+	public GregorianCalendar getDatum() {
+		return datum;
+	}
+	public void setDatum(GregorianCalendar datum) {
+		this.datum = datum;
+	}
+	
+	
+	
+	
 }
