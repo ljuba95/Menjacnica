f8089e33d062fe25464727b019abd6cad807ef05
ljuba95 <ljuba95@hotmail.com>
2016-03-18 13:05:14 +0100

    Implementirane metode u klasi Menjacnica

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,26 +1,30 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import specifikacijainterfejsa.MenjacnicaInterface;
 
 public class Menjacnica implements MenjacnicaInterface {
 
+	LinkedList<Valuta> valute = new LinkedList<>();
 	@Override
 	public void dodajValutu(Valuta valuta) {
-		// TODO Auto-generated method stub
-
+		valute.add(valuta);
 	}
 
 	@Override
 	public void obrisiValutu(Valuta valuta) {
-		// TODO Auto-generated method stub
+		valute.remove(valuta);
 
 	}
 
 	@Override
 	public Valuta pronadjiValutu(String ime, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < valute.size(); i++) {
+			if(valute.get(i).getNaziv().equals(ime) && valute.get(i).getDatum().equals(datum))
+				return valute.get(i);
+		}
 		return null;
 	}
 
