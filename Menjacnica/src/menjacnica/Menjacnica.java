2c754e7e83b233baec915a134893cd6316edef7c
ljuba95 <ljuba95@hotmail.com>
2016-03-18 12:59:58 +0100

    Dodata klasa Menjacnica koja implementira MenjacnicaInterface

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,27 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import specifikacijainterfejsa.MenjacnicaInterface;
+
+public class Menjacnica implements MenjacnicaInterface {
+
+	@Override
+	public void dodajValutu(Valuta valuta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiValutu(Valuta valuta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Valuta pronadjiValutu(String ime, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
