1517b9a20adff271ec3d20f3795717a3d6ca2dfa
ljuba95 <ljuba95@hotmail.com>
2016-03-18 13:13:36 +0100

    Implementirana klasa Menjacnica

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,26 +1,38 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import specifikacijainterfejsa.MenjacnicaInterface;
 
 public class Menjacnica implements MenjacnicaInterface {
 
+	private LinkedList<Valuta> valute = new LinkedList<>();
 	@Override
 	public void dodajValutu(Valuta valuta) {
-		// TODO Auto-generated method stub
+		if(valuta != null)
+			valute.add(valuta);
+		else
+			throw new RuntimeException("Valuta ne sme biti null.");
 
 	}
 
 	@Override
 	public void obrisiValutu(Valuta valuta) {
-		// TODO Auto-generated method stub
+		if(valuta != null)
+			valute.remove(valuta);
+		else
+			throw new RuntimeException("Valuta ne sme biti null.");
 
 	}
 
 	@Override
 	public Valuta pronadjiValutu(String ime, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		for (Valuta valuta : valute) {
+			if((valuta.getNaziv().equals(ime) || valuta.getKraciNaziv().equals(ime))
+					&& valuta.getDatum().equals(datum))
+				return valuta;
+		}
 		return null;
 	}
 
